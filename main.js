0b8f868a41c4dc9133c4d587cadb207fbad9f74d
matteo belcari <matteobelcari977@gmail.com>
2022-02-04 16:26:56 +0100

    milestone 1 finito

---
+++ main.js
@@ -0,0 +1,137 @@
+const icons = [
+	{
+		name: 'cat',
+		prefix: 'fa-',
+		type: 'cat',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'crow',
+		prefix: 'fa-',
+		type: 'crow',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'dog',
+		prefix: 'fa-',
+		type: 'dog',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'dove',
+		prefix: 'fa-',
+		type: 'dove',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'dragon',
+		prefix: 'fa-',
+		type: 'dragon',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'horse',
+		prefix: 'fa-',
+		type: 'horse',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'hippo',
+		prefix: 'fa-',
+		type: 'hippo',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'fish',
+		prefix: 'fa-',
+		type: 'fish',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'carrot',
+		prefix: 'fa-',
+		type: 'carrot',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'apple-alt',
+		prefix: 'fa-',
+		type: 'apple-alt',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'lemon',
+		prefix: 'fa-',
+		type: 'lemon',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'pepper-hot',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user-astronaut',
+		family: 'fas',
+		color: 'purple'
+	},
+	{
+		name: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user-graduate',
+		family: 'fas',
+		color: 'purple'
+	},
+	{
+		name: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user-ninja',
+		family: 'fas',
+		color: 'purple'
+	},
+	{
+		name: 'user-secret',
+		prefix: 'fa-',
+		type: 'user-secret',
+		family: 'fas',
+		color: 'purple'
+	}
+];
+
+const container = document.getElementById('container-icons');
+putIcons(container, icons)
+
+function putIcons(container, icons){
+
+    let content = '';
+
+    // uso il ciclo forEach per iniettare le mie icone all'interno dell'html facendo riferimento all'array icons
+
+    icons.forEach(elemento => {
+
+        content +=`<div class="icons">
+                    <i style="color:${elemento.color}" class="${elemento.family} ${elemento.prefix}${elemento.type}"></i>
+                    <div class="icons-text">${elemento.name}</div>
+                </div>`
+        
+    });
+
+    container.innerHTML = content;
+
+    console.log(content)
+}
