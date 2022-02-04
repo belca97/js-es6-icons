a391b698a60fe284a773c250094bcb71dcfb9c9a
matteo belcari <matteobelcari977@gmail.com>
2022-02-04 16:52:54 +0100

    Fine milestone 3

--- main.js
+++ main.js
@@ -2,112 +2,112 @@ const icons = [
 	{
 		name: 'cat',
 		prefix: 'fa-',
-		type: 'cat',
+		type: 'Animali',
 		family: 'fas',
 		color: 'blue'
 	},
 	{
 		name: 'crow',
 		prefix: 'fa-',
-		type: 'crow',
+		type: 'Animali',
 		family: 'fas',
 		color: 'blue'
 	},
 	{
 		name: 'dog',
 		prefix: 'fa-',
-		type: 'dog',
+		type: 'Animali',
 		family: 'fas',
 		color: 'blue'
 	},
 	{
 		name: 'dove',
 		prefix: 'fa-',
-		type: 'dove',
+		type: 'Animali',
 		family: 'fas',
 		color: 'blue'
 	},
 	{
 		name: 'dragon',
 		prefix: 'fa-',
-		type: 'dragon',
+		type: 'Animali',
 		family: 'fas',
 		color: 'blue'
 	},
 	{
 		name: 'horse',
 		prefix: 'fa-',
-		type: 'horse',
+		type: 'Animali',
 		family: 'fas',
 		color: 'blue'
 	},
 	{
 		name: 'hippo',
 		prefix: 'fa-',
-		type: 'hippo',
+		type: 'Animali',
 		family: 'fas',
 		color: 'blue'
 	},
 	{
 		name: 'fish',
 		prefix: 'fa-',
-		type: 'fish',
+		type: 'Animali',
 		family: 'fas',
 		color: 'blue'
 	},
 	{
 		name: 'carrot',
 		prefix: 'fa-',
-		type: 'carrot',
+		type: 'Vegetali',
 		family: 'fas',
 		color: 'orange'
 	},
 	{
 		name: 'apple-alt',
 		prefix: 'fa-',
-		type: 'apple-alt',
+		type: 'Vegetali',
 		family: 'fas',
 		color: 'orange'
 	},
 	{
 		name: 'lemon',
 		prefix: 'fa-',
-		type: 'lemon',
+		type: 'Vegetali',
 		family: 'fas',
 		color: 'orange'
 	},
 	{
 		name: 'pepper-hot',
 		prefix: 'fa-',
-		type: 'pepper-hot',
+		type: 'Vegetali',
 		family: 'fas',
 		color: 'orange'
 	},
 	{
 		name: 'user-astronaut',
 		prefix: 'fa-',
-		type: 'user-astronaut',
+		type: 'Utente',
 		family: 'fas',
 		color: 'purple'
 	},
 	{
 		name: 'user-graduate',
 		prefix: 'fa-',
-		type: 'user-graduate',
+		type: 'Utente',
 		family: 'fas',
 		color: 'purple'
 	},
 	{
 		name: 'user-ninja',
 		prefix: 'fa-',
-		type: 'user-ninja',
+		type: 'Utente',
 		family: 'fas',
 		color: 'purple'
 	},
 	{
 		name: 'user-secret',
 		prefix: 'fa-',
-		type: 'user-secret',
+		type: 'Utente',
 		family: 'fas',
 		color: 'purple'
 	}
@@ -115,19 +115,21 @@ const icons = [
 
 // faccio riferimento all'id container dell'html
 const container = document.getElementById('container-icons');
-// uso la funzione putIcons per creare le icone con colori e figure giuste
-putIcons(container, icons)
 
+
+
+//creo la funzione putIcons per creare le icone con colori e figure giuste
 function putIcons(container, icons){
 
     let content = '';
 
-    // uso il ciclo forEach per iniettare le mie icone all'interno dell'html facendo riferimento all'array icons
+    // uso il ciclo forEach per ciclare tutto il mio array e 
+    //prendere le chiavi per la sintassi delle icone e le chiavi per il testo
 
     icons.forEach(elemento => {
 
         content +=`<div class="icons">
-                    <i style="color:${elemento.color}" class="${elemento.family} ${elemento.prefix}${elemento.type}"></i>
+                    <i style="color:${elemento.color}" class="${elemento.family} ${elemento.prefix}${elemento.name}"></i>
                     <div class="icons-text">${elemento.name}</div>
                 </div>`
         
@@ -135,5 +137,34 @@ function putIcons(container, icons){
 
     container.innerHTML = content;
 
-    console.log(content)
+    console.log(content);
 }
+
+//faccio riferimento al selettore presente nell'html
+
+const selettori = document.getElementById('type-filter');
+
+putIcons(container, icons)
+
+// creo una funzione che mi consenta di filtrare le varie card a seconda del loro tipo
+selettori.addEventListener('change', function(){
+
+    let opzioni = this.value;
+    if(opzioni == ""){
+        putIcons(container, icons)
+    } else{
+
+        //uso la funzione filter per filtrare i valori di cui ho bisogno
+        const filtraggio = icons.filter(icons => {
+            if (icons.type == opzioni){
+                return true;
+            } 
+
+            return false;
+            
+        })
+
+        putIcons(container, filtraggio)
+    }
+
+})
